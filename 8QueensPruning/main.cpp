4f5be43da1c478a908ef9799042d5c28168e8b65
James <jgbentley10@gmail.com>
2015-08-24 09:43:55 -0400

    Initial Commit

---
+++ 8QueensPruning/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  8QueensPruning
+//
+//  Created by James Bentley on 8/24/15.
+//  Copyright (c) 2015 James Bentley. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
